a4f215a1dae95e49d7fa9f90bf8c259c4e83d24e
NafunNebula <nathan.bullock00@gmail.com>
2021-08-10 14:57:47 +0100

    added aditional operators

--- NafunCalculator/Calculator.py
+++ NafunCalculator/Calculator.py
@@ -1,10 +1,11 @@
+import math
 from tkinter import *
 
 root = Tk()
 root.title("Nafun Calculator")
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=15, pady=15)
+e = Entry(root, width=50, borderwidth=5)
+e.grid(row=0, column=0, columnspan=4, padx=15, pady=15)
 
 def addEntryValue(number):
     current = e.get()
@@ -16,7 +17,6 @@ def clearEntryValue():
 
 def solveEquation():
     result = eval(e.get())
-    print(result)
     e.delete(0, END)
     e.insert(0, result)
 
@@ -30,29 +30,42 @@ button7 = Button(root, text="7", padx=50, pady=30, command=lambda: addEntryValue
 button8 = Button(root, text="8", padx=50, pady=30, command=lambda: addEntryValue(8))
 button9 = Button(root, text="9", padx=50, pady=30, command=lambda: addEntryValue(9))
 button0 = Button(root, text="0", padx=50, pady=30, command=lambda: addEntryValue(0))
+
 buttonAdd = Button(root, text="+", padx=50, pady=30, command=lambda: addEntryValue('+'))
 buttonSubtract = Button(root, text="-", padx=50, pady=30, command=lambda: addEntryValue('-'))
+buttonMultiply = Button(root, text="*", padx=50, pady=30, command=lambda: addEntryValue('*'))
+buttonDecimal = Button(root, text=".", padx=50, pady=30, command=lambda: addEntryValue('.'))
 buttonEqual = Button(root, text="=", padx=50, pady=30, command=solveEquation)
 buttonClear = Button(root, text="C", padx=50, pady=30, command=clearEntryValue)
+buttonLeftBracket = Button(root, text="(", padx=50, pady=30, command=lambda: addEntryValue('('))
+buttonRightBracket = Button(root, text=")", padx=50, pady=30, command=lambda: addEntryValue(')'))
+buttonPi = Button(root, text="π", padx=50, pady=30, command=lambda: addEntryValue(math.pi))
+buttonDivide = Button(root, text="÷", padx=50, pady=30, command=lambda: addEntryValue('/'))
 
-button1.grid(row=3, column=0)
-button2.grid(row=3, column=1)
-button3.grid(row=3, column=2)
+buttonClear.grid(row=5, column=0)
+button0.grid(row=5, column=1)
+buttonDecimal.grid(row=5, column=2)
+buttonEqual.grid(row=5, column=3)
 
-button4.grid(row=2, column=0)
-button5.grid(row=2, column=1)
-button6.grid(row=2, column=2)
+button1.grid(row=4, column=0)
+button2.grid(row=4, column=1)
+button3.grid(row=4, column=2)
+buttonAdd.grid(row=4, column=3)
 
-button7.grid(row=1, column=0)
-button8.grid(row=1, column=1)
-button9.grid(row=1, column=2)
+button4.grid(row=3, column=0)
+button5.grid(row=3, column=1)
+button6.grid(row=3, column=2)
+buttonSubtract.grid(row=3, column=3)
 
-button0.grid(row=4, column=0)
-buttonAdd.grid(row=4, column=1)
-buttonSubtract.grid(row=4, column=2)
+button7.grid(row=2, column=0)
+button8.grid(row=2, column=1)
+button9.grid(row=2, column=2)
+buttonMultiply.grid(row=2, column=3)
 
-buttonEqual.grid(row=5, column=0)
-buttonClear.grid(row=5, column=1)
+buttonLeftBracket.grid(row=1, column=0)
+buttonRightBracket.grid(row=1, column=1)
+buttonPi.grid(row=1, column=2)
+buttonDivide.grid(row=1, column=3)
 
 root.mainloop()
 
